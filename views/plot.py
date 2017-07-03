15c8ada277ce2e0c345898c2d371ead5a08f8fea
Brian <brianmuse33@gmail.com>
2017-07-02 20:47:28 -0700

    Updated the Plot class.
    The plot class now creates a matplotlib plot with random data using the FigureCanvas class.

--- views/plot.py
+++ views/plot.py
@@ -1,13 +1,32 @@
 
+from PyQt5.QtWidgets import QSizePolicy
 
-class Plot:
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
-    def __init__(self, plot):
-        self.plot = plot
+import random
 
+class Plot(FigureCanvas):
+ 
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+ 
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+ 
+        FigureCanvas.setSizePolicy(self,
+                QSizePolicy.Expanding,
+                QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.plot()
+ 
+ 
+    def plot(self):
+        data = [random.random() for i in range(25)]
+        ax = self.figure.add_subplot(111)
+        ax.clear()
+        ax.plot(data, 'r-')
+        ax.set_title('PyQt Matplotlib Example')
+        self.draw()
 
-    def show(self):
-        self.plot.show()
-
-    def title(self, title):
-        self.plot.title(title)
