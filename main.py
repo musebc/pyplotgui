f3d2bece0c67baf327df414c647d4607484f0d3e
Brian <brianmuse33@gmail.com>
2017-07-02 20:49:42 -0700

    Updated the main window.
    This removes unused imports and updates the main window to use the plot helper class to create a new plot in the window. It also ads a push button that allows me to create a new plot of data.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 """The main window for the PyPlot GUI"""
 
 import sys
-from PyQt5.QtWidgets import QAction, QApplication, QMainWindow, QWidget
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout
 from PyQt5.QtGui import QIcon
 
+from views.plot import Plot
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 class MainWindow(QMainWindow):
     """Builds the main window of the PyPlot GUI"""
@@ -14,10 +18,15 @@ class MainWindow(QMainWindow):
 
     def init_ui(self):
         """Initialize the UI screen."""
-        self.setWindowTitle('Icon')
-        self.setWindowIcon(QIcon('views/smalllogo.png'))
-        self.statusBar().showMessage('Ready')
 
+        m = Plot(self, width=5, height=4)
+        m.move(0,0)
+ 
+        button = QPushButton('Plot Data', self)
+        button.setToolTip('Creates a new plot of data.')
+        button.clicked.connect(m.plot)
+        button.move(500,0)
+        button.resize(140,100)
         self.showMaximized()
 
 
